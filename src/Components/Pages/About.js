54a50654d73403ca5c54c543f31b457e8c752999
adnankhan2 <waheed.adnan@yahoo.com>
2020-04-13 00:09:19 +0300

    userUI added a lil but

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -3,9 +3,10 @@ import React ,{Fragment } from 'react'
 const About = () => {
     return (
         <Fragment>
-            <h1>About</h1>
+            <h1><em>A</em>bout</h1>
             <p>This is an app to search for GitHub Users</p>
             <p>Version 1.0.0</p>
+            <p><i class="fa fa-copyright" aria-hidden="true"></i>adnanwaheed 2020</p>
         </Fragment>
     )
 }
